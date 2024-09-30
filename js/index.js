4ef79f83fdb43d548678c4ec875e9347cf846e70
Tyler M. <tjmcdowell726@gmail.com>
2024-09-30 09:39:01 -0400

    Created functions for user and computer choice

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 const prompt = require('prompt-sync')();
+let userChoice = "";
+let computerChoice = "";
+
+function computerChoiceGeneration() {
+    let options = ["rock", "paper", "scissors"];
+    let rng = Math.floor(Math.random() * 3) + 1;
+    return options[rng];
+}
+
+function rockSelection() {
+    userChoice = "rock";
+    console.log("User's Choice: Rock");
+}
+function paperSelection() {
+    userChoice = "paper";
+}
+function scissorsSelection() {
+    userChoice = "scissors";
+}
+
+function whoWon(){
+    computerChoice = computerChoiceGeneration();
+    if (userChoice === computerChoice) {
+        console.log("Tie");
+        displayResult("Tie");
+    }
+}
